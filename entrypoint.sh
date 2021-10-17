64bad7ce3be824a777e6574d0fe1919460e57a3c
Rhys Newell <42852939+rhysnewell@users.noreply.github.com>
2021-10-17 17:27:50 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+version=3.21
+build=2
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+./bootstrap
+make -j$(nproc)
+make install
 cmake --version
+cd ../
+
 
 if [ "$1" == "build" ]; then
   args="--target x86_64-unknown-linux-musl --release $2"
