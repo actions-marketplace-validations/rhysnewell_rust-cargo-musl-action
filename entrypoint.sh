f62493df2b5b03dda78141973c0988a8c9e4ad9d
Rhys Newell <42852939+rhysnewell@users.noreply.github.com>
2021-10-17 18:34:33 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-version=3.21
-build=2
-wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
-tar -xzvf cmake-$version.$build.tar.gz
-cd cmake-$version.$build/
-./bootstrap
-make -j$(nproc)
-make install
-cmake --version
-cd ../
+# version=3.21
+# build=2
+# wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+# tar -xzvf cmake-$version.$build.tar.gz
+# cd cmake-$version.$build/
+# ./bootstrap
+# make -j$(nproc)
+# make install
+# cmake --version
+# cd ../
 
 
 if [ "$1" == "build" ]; then
